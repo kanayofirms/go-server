b95d13145a1cf35caa3b70fe46ecfaa6d66e5e6f
kanayofirms <kanyeze@gmail.com>
2023-07-05 07:23:38 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r "http.Request") {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprint(w, "ParseForm() err: %v", err)
+		return
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r "http.Request") {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET"{
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprint(w, "hello!")
+}
+
+func main() {
+	fileServer := http.fileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server on port 8080\n")
+	if err := http.ListenAndServe("8080", nil); err !=nil {
+		log.Fatal(err)
+	}
+}
