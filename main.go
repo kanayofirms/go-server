08753fbb793575b2db01be0720265beb70109732
kanayofirms <kanyeze@gmail.com>
2023-07-05 21:57:47 +0100

    Refactor code for functionality

--- main.go
+++ main.go
@@ -5,15 +5,20 @@ import(
 	"log"
 	"net/http"
 )
-/*
-func formHandler(w http.ResponseWriter, r "http.Request") {
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprint(w, "ParseForm() err: %v", err)
 		return
 	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
-*/
-func helloHandler(w http.ResponseWriter, r "http.Request") {
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
